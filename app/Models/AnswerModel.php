b7d1b6a16991ab9d37b762d5149837223658831e
kevinanggaaa <kevin.anggahung@gmail.com>
2020-07-09 21:11:23 +0700

    Create AnswerModel.php

---
+++ app/Models/AnswerModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\DB;
+
+class PertanyaanModel{
+    public static function upvotes($id){
+  
+    }
+
+    public static function downvotes($id){
+
+    }
+    
+}
+
+?>
