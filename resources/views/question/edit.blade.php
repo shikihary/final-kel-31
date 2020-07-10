df751e990ef75e49aee71425bd9c8658f9e7f686
Ghifari <ghifari226@gmail.com>
2020-07-10 13:52:24 +0700

    add TinyMCE editor

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('adminlte.master')
-
+@push('script-head')
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+@endpush
 @section('content')
   <div class="ml-3 mt-3">
     <div class="card card-primary">
@@ -19,7 +21,10 @@
           </div>
           <div class="form-group">
             <label for="isi">Isi</label>
+            <!-- Text Area Lama
             <textarea class="form-control" rows="5" id="isi" name="isi" placeholder="Input Isi">{{$question->isi}}</textarea>
+            -->
+            <textarea name="isi" class="form-control my-editor">{{$question->isi}}</textarea>
           </div>
         </div>
         <!-- /.card-body -->
@@ -31,3 +36,42 @@
     </div>
   </div>
 @endsection
+
+@push('scripts')
+    <script>
+    var editor_config = {
+        path_absolute : "/",
+        selector: "textarea.my-editor",
+        plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+        "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+        relative_urls: false,
+        file_browser_callback : function(field_name, url, type, win) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+        if (type == 'image') {
+            cmsURL = cmsURL + "&type=Images";
+        } else {
+            cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.open({
+            file : cmsURL,
+            title : 'Filemanager',
+            width : x * 0.8,
+            height : y * 0.8,
+            resizable : "yes",
+            close_previous : "no"
+        });
+        }
+    };
+
+    tinymce.init(editor_config);
+    </script>
+@endpush
