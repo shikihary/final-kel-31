e8f332f957b291cc866492ff8bdee4e18ee6c177
Ghifari <ghifari226@gmail.com>
2020-07-11 23:00:27 +0700

    add voteModels

--- app/Http/Controllers/AnswerVoteController.php
+++ app/Http/Controllers/AnswerVoteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
-use App\Answer_vote;
+use App\Models\Answer_votesModel;
 
 class AnswerVoteController extends Controller
 {
--- app/Http/Controllers/QuestionVoteController.php
+++ app/Http/Controllers/QuestionVoteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
-use App\Question_vote;
+use App\Models\Question_votesModel;
 
 class QuestionVoteController extends Controller
 {
