b63c0c795ca7bd411c683d7e47e57688d1c2d00e
Ghifari <ghifari226@gmail.com>
2020-07-11 22:51:08 +0700

    fix form user_id, add voteController

---
+++ app/Http/Controllers/AnswerVoteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+use App\Answer_vote;
+
+class AnswerVoteController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/QuestionVoteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+use App\Question_vote;
+
+class QuestionVoteController extends Controller
+{
+    //
+}
